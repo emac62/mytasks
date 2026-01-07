f1ba8edff87ad481a8eecb46d12a541fb07051c3
emac62 <borderlineboomer@gmail.com>
2026-01-07 11:30:42 -0500

    Initial Commit

---
+++ MyTasks/MyTasksApp.swift
@@ -0,0 +1,21 @@
+//
+//  MyTasksApp.swift
+//  MyTasks
+//
+//  Created by Ellen McConomy on 2026-01-07.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct MyTasksApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
